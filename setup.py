3cbbb582b1aae4ae78570bf3e2943f3c32d7991c
Okko Makkonen <okko.makkonen@aalto.fi>
2019-06-09 17:21:52 +0300

    Setup muutoksia

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='dmath',
       description='Module for math with dual numbers including automatic differentiation',
       version='0.2',
       packages=['dmath'],
-      author='Okko Makkonen'
+      author='Okko Makkonen',
+      test_suite='tests'
      )
